0df09eedb92c612c8bbbffe3ef594f3280678e82
MonaAbdelaziz2024 <monamohamedabdelazizmohamed@gmail.com>
2024-02-15 03:16:43 +0200

    add custom app bar and text fields and buttons to edit view .

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_button.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          CustomAppBar(title: 'Edit Note',icon: Icons.check,),
+          SizedBox(
+              height: 32,
+            ),
+            CustomTextField( hintText: 'Title',),
+            SizedBox(height: 16,),
+            CustomTextField(hintText: 'Content' , maxLines: 5,),
+            SizedBox(height: 32,),
+            CustomButton(),
+            SizedBox(height: 16,),
+      ],),);
   }
 }
